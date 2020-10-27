fa4fc00756e67467ddd3c5c24939d9a2eb20f721
Shin seongsu <tlstjdtn321@naver.com>
2020-10-28 00:30:22 +0900

    타입스크립트 기본 설정해보기

---
+++ ch6/src/index.ts
@@ -0,0 +1,14 @@
+console.log('Hello TypeScript!')
+
+// dist 폴더에 javascript 컴파일
+// ./node_modules/.bin/tsc
+
+
+let a = 1 + 2
+let b = a + 3
+let c = {
+    apple: a,
+    banana: b
+}
+let d = c.apple * 4
+
