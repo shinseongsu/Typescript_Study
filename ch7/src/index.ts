d18f2931606c6a79d05c374a4ca30ddf79a3face
Shin seongsu <tlstjdtn321@naver.com>
2020-10-28 00:31:15 +0900

    타입스크립트 타입 공부(any, unknown)

---
+++ ch7/src/index.ts
@@ -0,0 +1,24 @@
+function squareOf(n: number) {
+    return n * n
+}
+
+squareOf(2)
+// squareOf('z')   // 에러 "z"라는 타입의 인수는 'number' 타입의 매개변수에 할당할 수 없음
+
+
+// any는 뭐든 할 수있지만 꼭 필요한 상황이 아니라면 사용하지 않는 것이 좋다.
+let a: any = 666
+let b: any = ['danger']
+let c: any = a + b
+
+// unknown
+// 타입을 미리 알 수 없는 어떤 값이 있을 때 any 대신 unknown을 사용하자.
+// unknown의 타입을 검사해 타입스크립트가 unknown 타입의 값을 사용할 수 없게 강제한다.
+
+let a1: unknown = 30    // unknown
+let b1 = a1 === 123     // boolean
+let c1 = a1 + 10        // 에러 객체 타입이 unknown 임.
+
+if ( typeof a === 'number' ) {
+    let d = a + 10      // number
+}
