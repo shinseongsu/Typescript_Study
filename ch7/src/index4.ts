3795e4d9996dbaedac63dcb954d665f3fe3cca85
Shin seongsu <tlstjdtn321@naver.com>
2020-10-29 14:13:59 +0900

    string 타입

---
+++ ch7/src/index4.ts
@@ -0,0 +1,10 @@
+// string
+// string은 모든 문자열의 집합으로 연결(+), 슬라이스(.slice) 등의 연산을 수행 할 수 있다.
+
+let a5 = 'hello' // string
+let b5 = 'billy' // string 
+const c5 = '!'   // '!'
+let d5 = a5 + ' ' + b5 + c5 // string
+let e5: string = 'zoom' // string
+let f5: 'john' = 'john' // string
+// let g5: 'john' = 'zoe'  // 에러 "zoe" 타입을 "john" 타입에 할당할 수 없음
