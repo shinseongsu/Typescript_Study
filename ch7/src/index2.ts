a5563fcd73941c83f1b9cda0dcf1f55264d4098b
Shin seongsu <tlstjdtn321@naver.com>
2020-10-29 14:09:22 +0900

    number 타입

---
+++ ch7/src/index2.ts
@@ -0,0 +1,11 @@
+// number
+// number 타입은 모든 숫자(정수, 소수, 양수, 음수, Infinity, NaN 등)의 집합이다.
+// number 타입에는 덧셈(+), 뺄셈(-), 모듈로(%), 비교(<) 등의 숫자 관련 연산을 수행할 수 있다.
+
+let a3 = 1234    // number
+let b3 = Infinity * 0.10 // number
+const c3 = 5678     // 5678
+let d3 = a3 < b3    // boolean  
+let e3: number = 100    // number
+let f3: 26.218 = 26.218 // 26.218
+// let g3: 26.218 = 10 // 에러 '10' 타입을 '26.218' 타입에 할당할 수 없음
