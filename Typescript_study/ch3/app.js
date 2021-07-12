59c57b9cc7a5f65f78c123f79490b40bdda33e41
Shin seongsu <tlstjdtn321@naver.com>
2021-07-12 23:24:54 +0900

    parcel 사용하기

---
+++ Typescript_study/ch3/app.js
@@ -0,0 +1,6 @@
+const container = document.getElementById('root');
+const ajax = new XMLHttpRequest();
+const content = document.createElement('div');
+const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+
