78c19ec985e589903ec5d10eb9fd1ef3002333cf
Shin seongsu <tlstjdtn321@naver.com>
2021-07-12 23:49:24 +0900

    ajax 간단한거 해보기

--- Typescript_study/ch3/dist/app.c328ef1a.js
+++ Typescript_study/ch3/dist/app.c328ef1a.js
@@ -120,7 +120,35 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 })({"app.js":[function(require,module,exports) {
 var container = document.getElementById('root');
 var ajax = new XMLHttpRequest();
-},{}],"../../../../../../usr/local/share/.config/yarn/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var content = document.createElement('div');
+var NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+var CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+ajax.open('GET', NEWS_URL, false);
+ajax.send();
+var newsFeed = JSON.parse(ajax.response);
+var ul = document.createElement('ul');
+window.addEventListener('hashchange', function () {
+  var id = location.hash.substr(1);
+  ajax.open('GET', CONTENT_URL.replace('@id', id), false);
+  ajax.send();
+  var newsContent = JSON.parse(ajax.response);
+  var title = document.createElement('h1');
+  title.innerHTML = newsContent.title;
+  content.appendChild(title);
+});
+
+for (var i = 0; i < 10; i++) {
+  var li = document.createElement('li');
+  var a = document.createElement('a');
+  a.href = "".concat(newsFeed[i].id);
+  a.innerHTML = "".concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")");
+  li.appendChild(a);
+  ul.appendChild(li);
+}
+
+container.appendChild(ul);
+container.appendChild(content);
+},{}],"../../../../.nvm/versions/node/v15.11.0/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -148,7 +176,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50109" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49283" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -324,5 +352,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../../../../usr/local/share/.config/yarn/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","app.js"], null)
+},{}]},{},["../../../../.nvm/versions/node/v15.11.0/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","app.js"], null)
 //# sourceMappingURL=/app.c328ef1a.js.map
