8ff8cddaa61eebdb72f15b4a43525e25586e7535
Shin seongsu <tlstjdtn321@naver.com>
2021-08-19 01:48:04 +0900

    game 만들기

--- Typescript_make/dist/src.f10117fe.js
+++ Typescript_make/dist/src.f10117fe.js
@@ -117,7 +117,382 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"src/Game.ts":[function(require,module,exports) {
+})({"src/images/lion.png":[function(require,module,exports) {
+module.exports = "/lion.0a55027b.png";
+},{}],"src/images/chicken.png":[function(require,module,exports) {
+module.exports = "/chicken.3d0d4a2d.png";
+},{}],"src/images/griff.png":[function(require,module,exports) {
+module.exports = "/griff.78de84a7.png";
+},{}],"src/images/elophant.png":[function(require,module,exports) {
+module.exports = "/elophant.66e48f21.png";
+},{}],"src/Piece.ts":[function(require,module,exports) {
+"use strict";
+
+var __extends = this && this.__extends || function () {
+  var _extendStatics = function extendStatics(d, b) {
+    _extendStatics = Object.setPrototypeOf || {
+      __proto__: []
+    } instanceof Array && function (d, b) {
+      d.__proto__ = b;
+    } || function (d, b) {
+      for (var p in b) {
+        if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p];
+      }
+    };
+
+    return _extendStatics(d, b);
+  };
+
+  return function (d, b) {
+    if (typeof b !== "function" && b !== null) throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+
+    _extendStatics(d, b);
+
+    function __() {
+      this.constructor = d;
+    }
+
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+  };
+}();
+
+var __importDefault = this && this.__importDefault || function (mod) {
+  return mod && mod.__esModule ? mod : {
+    "default": mod
+  };
+};
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Chick = exports.Griff = exports.Elephant = exports.Lion = exports.MoveResult = void 0;
+
+var lion_png_1 = __importDefault(require("./images/lion.png"));
+
+var chicken_png_1 = __importDefault(require("./images/chicken.png"));
+
+var griff_png_1 = __importDefault(require("./images/griff.png"));
+
+var elophant_png_1 = __importDefault(require("./images/elophant.png"));
+
+var Player_1 = require("./Player");
+
+var MoveResult = function () {
+  function MoveResult(killedPiece) {
+    this.killedPiece = killedPiece;
+  }
+
+  MoveResult.prototype.getKilled = function () {
+    return this.killedPiece;
+  };
+
+  return MoveResult;
+}();
+
+exports.MoveResult = MoveResult;
+
+var DefaultPiece = function () {
+  function DefaultPiece(currentPosition, ownerType) {
+    this.currentPosition = currentPosition;
+    this.ownerType = ownerType;
+    this._dead = false;
+  }
+
+  DefaultPiece.prototype.move = function (from, to) {
+    if (!this.canMove(to.poisiton)) {
+      throw new Error("Can not move");
+    }
+
+    var moveResult = new MoveResult(to.getPiece() != null ? to.getPiece() : null);
+    to.put(this);
+    from.put(null);
+    this.currentPosition = to.poisiton;
+    return moveResult;
+  };
+
+  return DefaultPiece;
+}();
+
+var Lion = function (_super) {
+  __extends(Lion, _super);
+
+  function Lion() {
+    return _super !== null && _super.apply(this, arguments) || this;
+  }
+
+  Lion.prototype.canMove = function (pos) {
+    var canMove = pos.row === this.currentPosition.row + 1 && pos.col === this.currentPosition.col || pos.row === this.currentPosition.row - 1 && pos.col === this.currentPosition.col || pos.col === this.currentPosition.col + 1 && pos.row === this.currentPosition.row || pos.col === this.currentPosition.col - 1 && pos.row === this.currentPosition.row || pos.row === this.currentPosition.row + 1 && pos.col === this.currentPosition.col + 1 || pos.row === this.currentPosition.row + 1 && pos.col === this.currentPosition.col - 1 || pos.row === this.currentPosition.row - 1 && pos.col === this.currentPosition.col + 1 || pos.row === this.currentPosition.row - 1 && pos.col === this.currentPosition.col - 1;
+    return canMove;
+  };
+
+  Lion.prototype.render = function () {
+    return "<img class=\"piece " + this.ownerType + "\" src=\"" + lion_png_1.default + "\" width=\"90%\" height=\"90%\"/>";
+  };
+
+  return Lion;
+}(DefaultPiece);
+
+exports.Lion = Lion;
+
+var Elephant = function (_super) {
+  __extends(Elephant, _super);
+
+  function Elephant() {
+    return _super !== null && _super.apply(this, arguments) || this;
+  }
+
+  Elephant.prototype.canMove = function (pos) {
+    return pos.row === this.currentPosition.row + 1 && pos.col === this.currentPosition.col + 1 || pos.row === this.currentPosition.row + 1 && pos.col === this.currentPosition.col - 1 || pos.row === this.currentPosition.row - 1 && pos.col === this.currentPosition.col + 1 || pos.row === this.currentPosition.row - 1 && pos.col === this.currentPosition.col - 1;
+  };
+
+  Elephant.prototype.render = function () {
+    return "<img class=\"piece " + this.ownerType + "\" src=\"" + elophant_png_1.default + "\" width=\"90%\" height=\"90%\"/>";
+  };
+
+  return Elephant;
+}(DefaultPiece);
+
+exports.Elephant = Elephant;
+
+var Griff = function (_super) {
+  __extends(Griff, _super);
+
+  function Griff() {
+    return _super !== null && _super.apply(this, arguments) || this;
+  }
+
+  Griff.prototype.canMove = function (pos) {
+    return pos.row === this.currentPosition.row + 1 && pos.col === this.currentPosition.col || pos.row === this.currentPosition.row - 1 && pos.col === this.currentPosition.col || pos.col === this.currentPosition.col + 1 && pos.row === this.currentPosition.row || pos.col === this.currentPosition.col - 1 && pos.row === this.currentPosition.row;
+  };
+
+  Griff.prototype.render = function () {
+    return "<img class=\"piece " + this.ownerType + "\" src=\"" + griff_png_1.default + "\" width=\"90%\" height=\"90%\"/>";
+  };
+
+  return Griff;
+}(DefaultPiece);
+
+exports.Griff = Griff;
+
+var Chick = function (_super) {
+  __extends(Chick, _super);
+
+  function Chick() {
+    return _super !== null && _super.apply(this, arguments) || this;
+  }
+
+  Chick.prototype.canMove = function (pos) {
+    return this.currentPosition.row + (this.ownerType == Player_1.PlayerType.UPPER ? +1 : -1) === pos.row;
+  };
+
+  Chick.prototype.render = function () {
+    return "<img class=\"piece " + this.ownerType + "\" src=\"" + chicken_png_1.default + "\" width=\"90%\" height=\"90%\"/>";
+  };
+
+  return Chick;
+}(DefaultPiece);
+
+exports.Chick = Chick;
+},{"./images/lion.png":"src/images/lion.png","./images/chicken.png":"src/images/chicken.png","./images/griff.png":"src/images/griff.png","./images/elophant.png":"src/images/elophant.png","./Player":"src/Player.ts"}],"src/Player.ts":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Player = exports.PlayerType = void 0;
+
+var Piece_1 = require("./Piece");
+
+var PlayerType;
+
+(function (PlayerType) {
+  PlayerType["UPPER"] = "UPPER";
+  PlayerType["LOWER"] = "LOWER";
+})(PlayerType = exports.PlayerType || (exports.PlayerType = {}));
+
+var Player = function () {
+  function Player(type) {
+    this.type = type;
+
+    if (type == PlayerType.UPPER) {
+      this.pieces = [new Piece_1.Griff({
+        row: 0,
+        col: 0
+      }, PlayerType.UPPER), new Piece_1.Lion({
+        row: 0,
+        col: 1
+      }, PlayerType.UPPER), new Piece_1.Elephant({
+        row: 0,
+        col: 2
+      }, PlayerType.UPPER), new Piece_1.Chick({
+        row: 1,
+        col: 1
+      }, PlayerType.UPPER)];
+    } else {
+      this.pieces = [new Piece_1.Elephant({
+        row: 3,
+        col: 0
+      }, PlayerType.LOWER), new Piece_1.Lion({
+        row: 3,
+        col: 1
+      }, PlayerType.LOWER), new Piece_1.Griff({
+        row: 3,
+        col: 2
+      }, PlayerType.LOWER), new Piece_1.Chick({
+        row: 2,
+        col: 1
+      }, PlayerType.LOWER)];
+    }
+  }
+
+  return Player;
+}();
+
+exports.Player = Player;
+},{"./Piece":"src/Piece.ts"}],"src/board.ts":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Deadzone = exports.Board = exports.Cell = void 0;
+
+var Player_1 = require("./Player");
+
+var Cell = function () {
+  function Cell(poisiton, piece) {
+    this.poisiton = poisiton;
+    this.piece = piece;
+    this.isActive = false;
+    this._el = document.createElement("DIV");
+
+    this._el.classList.add("cell");
+  }
+
+  Cell.prototype.put = function (piece) {
+    this.piece = piece;
+  };
+
+  Cell.prototype.getPiece = function () {
+    return this.piece;
+  };
+
+  Cell.prototype.active = function () {
+    this.isActive = true;
+  };
+
+  Cell.prototype.deactive = function () {
+    this.isActive = false;
+  };
+
+  Cell.prototype.render = function () {
+    if (this.isActive) {
+      this._el.classList.add("active");
+    } else {
+      this._el.classList.remove("active");
+    }
+
+    if (this.getPiece() && this.getPiece().ownerType === Player_1.PlayerType.UPPER) {
+      this._el.classList.add("upper");
+    } else {
+      this._el.classList.remove("upper");
+    }
+
+    this._el.innerHTML = this.piece ? this.piece.render() : "";
+  };
+
+  return Cell;
+}();
+
+exports.Cell = Cell;
+
+var Board = function () {
+  function Board(upperPlayer, lowerPlayer) {
+    this.cells = [];
+    this.el = document.createElement("div");
+    this.map = new WeakMap();
+    this.el.className = "board";
+
+    var _loop_1 = function _loop_1(row) {
+      var rowEl = document.createElement("div");
+      rowEl.className = "row";
+      this_1.el.appendChild(rowEl);
+
+      var _loop_2 = function _loop_2(col) {
+        var piece = upperPlayer.pieces.find(function (_a) {
+          var currentPosition = _a.currentPosition;
+          return currentPosition.col === col && currentPosition.row === row;
+        }) || lowerPlayer.pieces.find(function (_a) {
+          var currentPosition = _a.currentPosition;
+          return currentPosition.col === col && currentPosition.row === row;
+        });
+        var cell = new Cell({
+          row: row,
+          col: col
+        }, piece);
+        this_1.map.set(cell._el, cell);
+        this_1.cells.push(cell);
+        rowEl.appendChild(cell._el);
+      };
+
+      for (var col = 0; col < 3; col++) {
+        _loop_2(col);
+      }
+    };
+
+    var this_1 = this;
+
+    for (var row = 0; row < 4; row++) {
+      _loop_1(row);
+    }
+  }
+
+  Board.prototype.render = function () {
+    this.cells.forEach(function (v) {
+      return v.render();
+    });
+  };
+
+  return Board;
+}();
+
+exports.Board = Board;
+
+var Deadzone = function () {
+  function Deadzone(type) {
+    this.type = type;
+    this.cells = [];
+    this.deadzoneEl = document.getElementById(this.type + "_deadzone").querySelector(".card-body");
+
+    for (var index = 0; index < 4; index++) {
+      var cell = new Cell({
+        col: index,
+        row: 0
+      }, null);
+      this.cells.push(cell);
+      this.deadzoneEl.appendChild(cell._el);
+    }
+  }
+
+  Deadzone.prototype.add = function (piece) {
+    var emptyCell = this.cells.find(function (v) {
+      return v.getPiece() == null;
+    });
+    emptyCell.put(piece);
+    emptyCell.render();
+  };
+
+  Deadzone.prototype.render = function () {
+    this.cells.forEach(function (v) {
+      return v.render();
+    });
+  };
+
+  return Deadzone;
+}();
+
+exports.Deadzone = Deadzone;
+},{"./Player":"src/Player.ts"}],"src/Game.ts":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -125,14 +500,211 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.Game = void 0;
 
+var Player_1 = require("./Player");
+
+var board_1 = require("./board");
+
+var Piece_1 = require("./Piece");
+
 var Game = function () {
-  function Game() {}
+  function Game() {
+    var _this = this;
+
+    this.turn = 0;
+    this.gameInfoEl = document.querySelector(".alert");
+    this.state = "STARTED";
+    this.upperPlayer = new Player_1.Player(Player_1.PlayerType.UPPER);
+    this.lowerPlayer = new Player_1.Player(Player_1.PlayerType.LOWER);
+    this.board = new board_1.Board(this.upperPlayer, this.lowerPlayer);
+    this.upperDeadZone = new board_1.Deadzone(Player_1.PlayerType.UPPER);
+    this.lowerDeadZone = new board_1.Deadzone(Player_1.PlayerType.LOWER);
+    var boardContainer = document.querySelector(".board-container");
+    boardContainer.firstChild.remove();
+    boardContainer.appendChild(this.board.el);
+    this.currentUser = this.upperPlayer;
+    this.board.render();
+    this.renderInfo();
+    this.board.el.addEventListener("click", function (e) {
+      if (_this.state === "END") {
+        return false;
+      }
+
+      if (e.target instanceof HTMLElement) {
+        var cellEl = void 0;
+
+        if (e.target.classList.contains("cell")) {
+          cellEl = e.target;
+        } else if (e.target.classList.contains("piece")) {
+          cellEl = e.target.parentElement;
+        } else {
+          return false;
+        }
+
+        var cell = _this.board.map.get(cellEl);
+
+        if (_this.isCurrentUserPiece(cell)) {
+          _this.select(cell);
+
+          return false;
+        }
+
+        if (_this.selectedCell) {
+          _this.move(cell);
+
+          _this.changeTurn();
+        } else if (!_this.selectedCell) {
+          return false;
+        } else {
+          alert("\uD604\uC7AC \uC720\uC800\uB294 " + _this.currentUser.type + " \uC785\uB2C8\uB2E4.");
+          return false;
+        }
+      }
+    });
+  }
+
+  Game.prototype.isCurrentUserPiece = function (cell) {
+    return cell != null && cell.getPiece() != null && cell.getPiece().ownerType === this.currentUser.type;
+  };
+
+  Game.prototype.select = function (cell) {
+    if (cell.getPiece() == null) {
+      return;
+    }
+
+    if (cell.getPiece().ownerType !== this.currentUser.type) {
+      return;
+    }
+
+    if (this.selectedCell) {
+      this.selectedCell.deactive();
+      this.selectedCell.render();
+    }
+
+    this.selectedCell = cell;
+    cell.active();
+    cell.render();
+  };
+
+  Game.prototype.move = function (cell) {
+    this.selectedCell.deactive();
+    var killed = this.selectedCell.getPiece().move(this.selectedCell, cell).getKilled();
+    this.selectedCell = cell;
+
+    if (killed) {
+      if (killed.ownerType === Player_1.PlayerType.UPPER) {
+        this.lowerDeadZone.add(killed);
+      } else {
+        this.upperDeadZone.add(killed);
+      }
+
+      if (killed instanceof Piece_1.Lion) {
+        this.state = "END";
+      }
+    }
+  };
+
+  Game.prototype.renderInfo = function (extraMessage) {
+    this.gameInfoEl.innerHTML = "#" + this.turn + "\uD134 " + this.currentUser.type + " \uCC28\uB840 " + (extraMessage ? "| " + extraMessage : "");
+  };
+
+  Game.prototype.changeTurn = function () {
+    this.selectedCell.deactive();
+    this.selectedCell = null;
+
+    if (this.state === "END") {
+      this.renderInfo("END! winner is " + this.currentUser.type);
+    } else {
+      this.turn += 1;
+      this.currentUser = this.currentUser === this.lowerPlayer ? this.upperPlayer : this.lowerPlayer;
+      this.renderInfo();
+    }
+
+    this.board.render();
+  };
 
   return Game;
 }();
 
 exports.Game = Game;
-},{}],"src/index.ts":[function(require,module,exports) {
+},{"./Player":"src/Player.ts","./board":"src/board.ts","./Piece":"src/Piece.ts"}],"node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
+var bundleURL = null;
+
+function getBundleURLCached() {
+  if (!bundleURL) {
+    bundleURL = getBundleURL();
+  }
+
+  return bundleURL;
+}
+
+function getBundleURL() {
+  // Attempt to find the URL of the current script and use that as the base URL
+  try {
+    throw new Error();
+  } catch (err) {
+    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
+
+    if (matches) {
+      return getBaseURL(matches[0]);
+    }
+  }
+
+  return '/';
+}
+
+function getBaseURL(url) {
+  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)?\/[^/]+(?:\?.*)?$/, '$1') + '/';
+}
+
+exports.getBundleURL = getBundleURLCached;
+exports.getBaseURL = getBaseURL;
+},{}],"node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
+var bundle = require('./bundle-url');
+
+function updateLink(link) {
+  var newLink = link.cloneNode();
+
+  newLink.onload = function () {
+    link.remove();
+  };
+
+  newLink.href = link.href.split('?')[0] + '?' + Date.now();
+  link.parentNode.insertBefore(newLink, link.nextSibling);
+}
+
+var cssTimeout = null;
+
+function reloadCSS() {
+  if (cssTimeout) {
+    return;
+  }
+
+  cssTimeout = setTimeout(function () {
+    var links = document.querySelectorAll('link[rel="stylesheet"]');
+
+    for (var i = 0; i < links.length; i++) {
+      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
+        updateLink(links[i]);
+      }
+    }
+
+    cssTimeout = null;
+  }, 50);
+}
+
+module.exports = reloadCSS;
+},{"./bundle-url":"node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"node_modules/bootstrap/dist/css/bootstrap.css":[function(require,module,exports) {
+
+        var reloadCSS = require('_css_loader');
+        module.hot.dispose(reloadCSS);
+        module.hot.accept(reloadCSS);
+      
+},{"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"src/styles/style.css":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
+
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
+},{"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"src/index.ts":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -141,8 +713,12 @@ Object.defineProperty(exports, "__esModule", {
 
 var Game_1 = require("./Game");
 
+require("bootstrap/dist/css/bootstrap.css");
+
+require("./styles/style.css");
+
 new Game_1.Game();
-},{"./Game":"src/Game.ts"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./Game":"src/Game.ts","bootstrap/dist/css/bootstrap.css":"node_modules/bootstrap/dist/css/bootstrap.css","./styles/style.css":"src/styles/style.css"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -170,7 +746,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50708" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56374" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
